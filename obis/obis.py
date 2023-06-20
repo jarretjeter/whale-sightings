e894e70dbe29b3287b8410f454c55963fa9585a3
jarretjeter <jarretjeter@yahoo.com>
2023-06-20 14:04:58 -0700

    define function to create and save dataframe to csv file

--- obis/obis.py
+++ obis/obis.py
@@ -14,14 +14,14 @@ logger: logging.Logger = logging
 obis_app = typer.Typer(no_args_is_help=True)
 
 
-def filter_keys(json_response: dict) -> list:
+def filter_keys(response: requests.Response) -> tuple:
     """
-    Remove irrelevant keys from returned json response
+    Remove irrelevant keys from returned response
     
     Args:
-        json_response: dict
+        response: `requests.Response`
     Returns:
-        list[dict]
+        tuple[list[dict], list]
     """
     # Relevant keys to keep
     key_list = [
@@ -59,26 +59,29 @@ def filter_keys(json_response: dict) -> list:
     'specificEpithet',
     'vernacularName'
     ]
-    response_list = json_response['results']
-    new_response_list = [{k:v for k, v in d.items() if k in key_list} for d in response_list if isinstance(d, dict)]
-    return new_response_list
+    response = response.json()
+    response_list = response['results']
+    filtered_response_list = [{k:v for k, v in d.items() if k in key_list} for d in response_list if isinstance(d, dict)]
+    return filtered_response_list, key_list
 
 
-def output_json(response: requests.Response, endpoint_name: str, whale: str, param: Optional[str]=None, start_date: Optional[str]=None, end_date: Optional[str]=None):
+def output_json(response: requests.Response, endpoint_name: str, whale: str, start_date: str, end_date: str, param: Optional[str]=None):
     """
     Save a `requests.Response` to a json file
 
     Args:
+        response: `requests.Response`
+            response object to read
+        endpoint_name: str
+            subdirectory name
         whale: str
             final subdirectory name
-        endpoint: str
-            subdirectory name
+        start_date: str
+            part of filename
+        end_date: str
+            part of filename
         param: (optional) str
             subdirectory name
-        start_date: (optional) str
-            part of filename
-        end_date: (optional) str
-            part of filename
     Returns:
         None
     """
@@ -89,27 +92,69 @@ def output_json(response: requests.Response, endpoint_name: str, whale: str, par
         output_dir = Path(f'{data_dir}/{endpoint_name}/{whale}')
 
     output_dir.mkdir(parents=True, exist_ok=True)
-    with open(f'{output_dir}/{start_date}-{end_date}.json', 'w') as file:
-        logger.info('Saving response to json file.')
+    with open(f'{output_dir}/{start_date}--{end_date}.json', 'w') as file:
+        logger.info(f"Saving response to json file: '{file.name}'")
         json.dump(response.json(), file, ensure_ascii=False, indent=4)
 
 
+def create_dataframe(response: requests.Response, endpoint_name: str, whale: str, start_date: str, end_date: str, param: Optional[str]=None):
+    """
+    Create a `pandas.DataFrame` from Obis API response
+
+    Args:
+        response: `requests.Response`
+            response object to read
+        endpoint_name: str
+            subdirectory name
+        whale: str
+            final subdirectory name
+        start_date: str
+            part of filename
+        end_date: str
+            part of filename
+        param: (optional) str
+            subdirectory name
+    Returns:
+        `pandas.DataFrame`
+    """
+    filtered = filter_keys(response)
+    response_list = filtered[0]
+    keys = filtered[1]
+    data_dir = './data'
+    if param != None:
+        output_dir = Path(f'{data_dir}/{endpoint_name}/{param}/{whale}')
+    else:
+        output_dir = Path(f'{data_dir}/{endpoint_name}/{whale}')
+
+    output_dir.mkdir(parents=True, exist_ok=True)
+    df = pd.json_normalize(response_list)
+    logger.info(f"Saving dataframe to csv: '{output_dir}/{start_date}--{end_date}.csv'")
+    df.to_csv(f'{output_dir}/{start_date}--{end_date}.csv', index=False)
+    return df
+
+
 @obis_app.command('obis_request')
-def obis_request(whale: str='blue_whale', endpoint: str='occurrence', param: Optional[str]=None, start_date: Optional[str]=None, end_date: Optional[str]=None, output: bool=True, size: Optional[int]=5000) -> requests.Response:
+def obis_request(whale: str, start_date: str, end_date: str, endpoint: str='occurrence', param: Optional[str]=None, json: bool=True, dataframe: bool=True, size: Optional[int]=5000) -> requests.Response:
     """
     Send a get request to the obis api (https://api.obis.org/v3) to find information of encounters with a specified whale species.
 
     Args:
         whale: str
             Whale to filter by
+        start_date: str
+            Date to start search in the format of `YYYY-MM-DD`
+        end_date: str
+            Date to end search in the format of `YYYY-MM-DD`
         endpoint: str
             API endpoint to request
         param: (optional) str
             endpoint parameter
-        start_date: (optional) str
-            Date to start search in the format of `YYYY-MM-DD`
-        end_date: (optional) str
-            Date to end search in the format of `YYYY-MM-DD`
+        json: bool, default True
+            Save response to a json file
+        dataframe: bool, default True
+            Save response to `pandas.DataFrame`
+        size: (optional) int
+            Maximum number of results allowed to be returned from response
     Returns:
         `requests.Response`
     """
@@ -133,11 +178,15 @@ def obis_request(whale: str='blue_whale', endpoint: str='occurrence', param: Opt
             url = url.replace(' ', '%20')
             r = requests.get(url)
         print(r.status_code)
-        if output:
-            output_json(response=r, endpoint_name=endpoint, whale=whale, param=param, start_date=start_date, end_date=end_date)
+        if json:
+            output_json(response=r, endpoint_name=endpoint, whale=whale, start_date=start_date, end_date=end_date, param=param)
+        if dataframe:
+            create_dataframe(response=r, endpoint_name=endpoint, whale=whale, start_date=start_date, end_date=end_date, param=param)
         return r
     except requests.exceptions.RequestException as e:
             logger.info(e)
+
+
 
 
 if __name__ == '__main__':
