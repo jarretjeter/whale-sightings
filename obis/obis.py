4435098b7018450c882e7546dfca84f1afe02a73
jarretjeter <jarretjeter@yahoo.com>
2023-07-24 13:12:22 -0700

    hardcode enpoint to occurrence and remove size param

--- obis/obis.py
+++ obis/obis.py
@@ -41,30 +41,23 @@ def filter_keys(response: requests.Response) -> tuple:
     return filtered_response_list, key_list
 
 
-def output_json(response: requests.Response, endpoint_name: str, whale: str, start_date: str, end_date: str, param: Optional[str]=None):
+def output_json(response: requests.Response, whale: str, start_date: str, end_date: str):
     """
     Save a `requests.Response` to a json file
 
     Args:
         response: `requests.Response`
             response object to read
-        endpoint_name: str
-            subdirectory name
         whale: str
             final subdirectory name
         start_date: str
             part of filename
         end_date: str
             part of filename
-        param: (optional) str
-            subdirectory name
     Returns:
         None
     """
-    if param != None:
-        output_dir = Path(f'{data_dir}/{endpoint_name}/{param}/{whale}')
-    else:
-        output_dir = Path(f'{data_dir}/{endpoint_name}/{whale}')
+    output_dir = Path(f'{data_dir}/{whale}')
 
     output_dir.mkdir(parents=True, exist_ok=True)
     with open(f'{output_dir}/{start_date}--{end_date}.json', 'w') as file:
@@ -113,33 +106,26 @@ def convert_dates(date_str: str) -> datetime.date:
         return f"Invalid date: {date_str}"
 
 
-def create_dataframe(response: requests.Response, endpoint_name: str, whale: str, start_date: str, end_date: str, param: Optional[str]=None):
+def create_dataframe(response: requests.Response, whale: str, start_date: str, end_date: str):
     """
     Create a `pandas.DataFrame` from Obis API response
 
     Args:
         response: `requests.Response`
             response object to read
-        endpoint_name: str
-            subdirectory name
         whale: str
             final subdirectory name
         start_date: str
             part of filename
         end_date: str
             part of filename
-        param: (optional) str
-            subdirectory name
     Returns:
         `pandas.DataFrame`
     """
     filtered = filter_keys(response)
     response_list = filtered[0]
     key_list = filtered[1]
-    if param != None:
-        output_dir = Path(f'{data_dir}/{endpoint_name}/{param}/{whale}')
-    else:
-        output_dir = Path(f'{data_dir}/{endpoint_name}/{whale}')
+    output_dir = Path(f'{data_dir}/{whale}')
 
     output_dir.mkdir(parents=True, exist_ok=True)
     df = pd.json_normalize(response_list)
@@ -153,31 +139,24 @@ def create_dataframe(response: requests.Response, endpoint_name: str, whale: str
     return df
 
 
-def merge_data(whale: str, endpoint_name: str='occurrence', param: Optional[str]=None) -> pd.DataFrame:
+def merge_data(whale: str) -> pd.DataFrame:
     """
     Create and save a merged dataframe from related csv files
     
     Args:
         whale: str
             whale species folder to filter by
-        endpoint_name: str
-            endpoint folder to filter by
-        param: str
-            endpoint parameter folder to filter by
     Returns:
         pd.DataFrame
     """
-    if param != None:
-        output_dir = Path(f'{data_dir}/{endpoint_name}/{param}/{whale}')
-    else:
-        output_dir = Path(f'{data_dir}/{endpoint_name}/{whale}')
+    output_dir = Path(f'{data_dir}/{whale}')
     
     csv_list = [file for file in output_dir.glob('*.csv')]
     df = pd.concat([pd.read_csv(csv) for csv in csv_list], ignore_index=True)
 
 
 @obis_app.command('obis_request')
-def obis_request(whale: str, start_date: str, end_date: str, endpoint: str='occurrence', param: Optional[str]=None, json: bool=True, dataframe: bool=True, size: Optional[int]=10000) -> requests.Response:
+def obis_request(whale: str, start_date: str, end_date: str, json: bool=True, dataframe: bool=True) -> requests.Response:
     """
     Send a get request to the obis api (https://api.obis.org/v3) to find information of encounters with a specified whale species.
 
@@ -188,43 +167,28 @@ def obis_request(whale: str, start_date: str, end_date: str, endpoint: str='occu
             Date to start search in the format of `YYYY-MM-DD`
         end_date: str
             Date to end search in the format of `YYYY-MM-DD`
-        endpoint: str
-            API endpoint to request
-        param: (optional) str
-            endpoint parameter
         json: bool, default True
             Save response to a json file
         dataframe: bool, default True
             Save response to `pandas.DataFrame`
-        size: (optional) int
-            Maximum number of results allowed to be returned from response. Limited to 10,000
     Returns:
         `requests.Response`
     """
     whale_list = {'blue_whale': {'scientific_name': 'Balaenoptera musculus'}, 'sperm_whale': {'scientific_name': 'Physeter macrocephalus'}}
-    endpoints = ['occurrence', 'statistics']
-    endpoint_params = {'occurrence': {'params': []}, 'statistics': {'params': ['years']}}
     assert whale in whale_list, f'Enter a whale from whale_list: {whale_list}'
-    assert endpoint in endpoints, f'Use a correct API endpoint: {endpoints}'
-    assert param in endpoint_params[endpoint]['params'] or param == None, f"Use a correct endpoint parameter: {endpoint_params}"
     
     api = 'https://api.obis.org/v3'
     try:
         scientific_name = whale_list[whale]['scientific_name']
         print(scientific_name)
-        if param != None:
-            url = f'{api}/{endpoint}/{param}?scientificname={scientific_name}&startdate={start_date}&enddate={end_date}&size={size}'
-            url = url.replace(' ', '%20')
-            r = requests.get(url)
-        else:
-            url = f'{api}/{endpoint}?scientificname={scientific_name}&startdate={start_date}&enddate={end_date}&size={size}'
-            url = url.replace(' ', '%20')
-            r = requests.get(url)
+        url = f'{api}/occurrence?scientificname={scientific_name}&startdate={start_date}&enddate={end_date}'
+        url = url.replace(' ', '%20')
+        r = requests.get(url)
         print(r.status_code)
         if json:
-            output_json(response=r, endpoint_name=endpoint, whale=whale, start_date=start_date, end_date=end_date, param=param)
+            output_json(response=r, whale=whale, start_date=start_date, end_date=end_date)
         if dataframe:
-            create_dataframe(response=r, endpoint_name=endpoint, whale=whale, start_date=start_date, end_date=end_date, param=param)
+            create_dataframe(response=r, whale=whale, start_date=start_date, end_date=end_date)
         return r
     except requests.exceptions.RequestException as e:
             logger.info(e)
