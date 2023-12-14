b0391c6670e559efe20174795b239cd125a5a66e
jarretjeter <jarretjeter@yahoo.com>
2023-12-14 00:04:38 -0800

    create separate API class, update main

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from storage import *
 import typer
-from typing import Optional
 from whalefinder import cleaner, obis, validate
 
 pipeline = typer.Typer()
 
 
-@pipeline.command('run')
-def run(whale: str, startdate: Optional[str]=None, enddate: Optional[str]=None):
+@pipeline.command('main')
+def main(whale: str, startdate: str='', enddate: str=''):
     """
     Pipeline orchestration
     """
-    obis_api = obis.ObisAPI(whale, startdate, enddate)
+    api = obis.ApiClient()
+    obis_api = obis.ObisHandler(api, whale, startdate, enddate)
     obis_api.api_requests()
     validator = validate.Validator(whale, startdate, enddate)
     valid_data, error_data = validator.validate_response()
