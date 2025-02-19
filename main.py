0b58e2471cde6ae0b7ca5d24f0b401541f0a66b1
jarretjeter <jarretjeter@yahoo.com>
2025-02-18 23:29:49 -0800

    create checkpoints for the pipeline script

--- main.py
+++ main.py
@@ -1,29 +1,59 @@
-from db import MySQLClient
 import typer
+
+from db import MySQLClient
 from whalefinder import cleaner, obis, validate
 
 pipeline = typer.Typer()
 
+#TODO add context data for pipeline
+
 
 @pipeline.command('main')
-def main(whale: str, startdate: str='', enddate: str=''):
+def main(whale: str, startdate: str='', enddate: str='') -> None:
     """
-    Pipeline orchestration
+    Full pipeline orchestration
     """
     api = obis.ApiClient()
     handler = obis.ObisHandler(api, whale, startdate, enddate)
     handler.batch_requests()
+
     validator = validate.Validator(whale, startdate, enddate)
     valid_data, error_data = validator.validate_response()
-    datacleaner = cleaner.WhaleDataCleaner(whale, valid_data, error_data, startdate, enddate)
-    df = datacleaner.process_and_save()
+    data_cleaner = cleaner.WhaleDataCleaner(whale, valid_data, error_data, startdate, enddate)
+    df = data_cleaner.process_and_save()
 
-    mysqlclient = MySQLClient()
-    with mysqlclient as client:
-        client.to_mysql(df)
+    mysql_client = MySQLClient()
+    mysql_client.to_mysql(df)
+
+# Checkpoints
+
+@pipeline.command('obis')
+def fetch_api(whale: str, startdate: str='', enddate: str='') -> None:
+    """
+    Only retrieve data from the Obis API
+    """
+    api = obis.ApiClient()
+    handler = obis.ObisHandler(api, whale, startdate, enddate)
+    handler.batch_requests()
+
+@pipeline.command('process')
+def validate_and_process(whale: str, startdate: str='', enddate: str='') -> None:
+    """
+    Data validations and transformations
+    """
+    validator = validate.Validator(whale, startdate, enddate)
+    valid_data, error_data = validator.validate_response()
+    data_cleaner = cleaner.WhaleDataCleaner(whale, valid_data, error_data, startdate, enddate)
+    data_cleaner.process_and_save()
+
+@pipeline.command('db')
+def load_to_db(data=None) -> None:
+    """
+    Load sighting data to database
+    """
+    mysql_client = MySQLClient()
+    mysql_client.to_mysql(data)
 
 
 if __name__ == '__main__':
-    print('Running pipeline')
     pipeline()
-    print('Pipeline finished')
